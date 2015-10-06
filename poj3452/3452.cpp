62b8918eddadebf72e0d4c5d4f324d19f5cad81c
Sykie Chen <sykiechen(at)gmail.com>
2015-10-06 22:23:01 +0800

    first ver with error

---
+++ poj3452/3452.cpp
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+void solve(int ctcars, int cttracks, int cars[]) {
+	int inorder[2000] = { -1 }, outorder[2000], trackidx = 0, tracklast[2000] = { 0 };
+
+	for (int i = 0; i < ctcars; i++) {
+		for (int j = 0; j < trackidx; j++) {
+			if (tracklast[j] == cars[i] - 1) {
+				inorder[i] = j;
+				tracklast[j]++;
+				outorder[cars[i]] = j;
+			}
+		}
+		if (inorder[i] == -1) {
+			if (trackidx < cttracks) {
+				inorder[i] = trackidx;
+				tracklast[trackidx] = cars[i];
+				outorder[cars[i]] = trackidx;
+				trackidx++;
+			}
+			else {
+				cout << "Transportation failed\n";
+				return;
+			}
+		}
+	}
+	for (int i = 0; i < ctcars; i++) cout << inorder[i] << ' ';
+	cout << '\n';
+	for (int i = 0; i < ctcars; i++) cout << outorder[i] << ' ';
+	cout << '\n';
+	return;
+}
+
+int main() {
+	int data[2000] = { 4,2,5,3,1 };
+	solve(5, 3, data);
+	return 0;
+}
