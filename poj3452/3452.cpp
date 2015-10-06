41dddc8a0ac81fe14fac479404f0d4156d45bc76
Sykie Chen <sykiechen(at)gmail.com>
2015-10-07 04:51:07 +0800

    Accepted!

--- poj3452/3452.cpp
+++ poj3452/3452.cpp
@@ -1,42 +1,102 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-void solve(int ctcars, int cttracks, int cars[]) {
-	int inorder[2000], outorder[2000], trackidx = 0, tracklast[2000] = { 0 };
+int cars[200000];
+
+int compare(const void *a, const void *b) {
+	return cars[*(int*)a] - cars[*(int*)b];
+}
+
+void solve(int ctcars, int cttracks) {
+	int *inorder = new int[200000];
+	int *outorder = new int[200000];
+	int *tracklast = new int[200000];
+	int trackidx = 0, minsp;
+
+	tracklast[0] = 0;
+
 
 	for (int i = 0; i < ctcars; i++) {
 		inorder[i] = -1;
+		outorder[i] = i;
+		minsp = 1<<30;
+		for (int j = 0; j <= trackidx; j++) {
+			if ((cars[i] - tracklast[j] < minsp)&&(cars[i] - tracklast[j] >= 0)){
+				minsp = cars[i] - tracklast[j];
+			}
+		}
+		//cout << minsp << '\n';
 		for (int j = 0; j < trackidx; j++) {
-			if (tracklast[j] == cars[i] - 1) {
+			if (tracklast[j] == cars[i] - minsp){
 				inorder[i] = j;
-				tracklast[j]++;
-				outorder[cars[i]] = j;
+				tracklast[j] = cars[i];
 			}
 		}
 		if (inorder[i] == -1) {
 			if (trackidx < cttracks) {
 				inorder[i] = trackidx;
 				tracklast[trackidx] = cars[i];
-				outorder[cars[i]] = trackidx;
 				trackidx++;
+				tracklast[trackidx] = 0;
 			}
 			else {
-				cout << "Transportation failed\n";
+				printf("Transportation failed\n");
+				delete[] inorder;
+				delete[] outorder;
+				delete[] tracklast;
 				return;
 			}
 		}
 	}
-	for (int i = 0; i < ctcars; i++) cout << inorder[i] << ' ';
-	cout << '\n';
-	for (int i = 0; i < ctcars; i++) cout << outorder[i] << ' ';
-	cout << '\n';
+	for (int i = 0; i < ctcars; i++) {
+		printf("%d",inorder[i] + 1);
+		if (i < ctcars - 1) printf(" ");
+	}
+	if (ctcars > 0) printf("\n");
+	qsort(outorder, ctcars, sizeof(int), compare);
+	for (int i = 0; i < ctcars; i++) {
+		printf("%d", inorder[outorder[i]] + 1);
+		if (i < ctcars - 1) printf(" ");
+	}
+	/*
+	sort(outorder, outorder + ctcars);
+	for (int i = 0; i < ctcars; i++) {
+		for (int j = 0; j < ctcars; j++) {
+			if (cars[j] == outorder[i]) {
+				printf("%d", inorder[j] + 1);
+				cars[j] = 0;
+				if (i < ctcars - 1) printf(" ");
+			}
+		}
+	}*/
+	if (ctcars > 0) printf("\n");
+	delete[] inorder;
+	delete[] outorder;
+	delete[] tracklast;
 	return;
 }
 
 int main() {
-	int data[2000] = { 4,2,5,3,1 };
-	solve(5, 3, data);
+	/*int size = 200;
+	int(*cars)[200000] = new int[size][200000];
+	int ctcars[200], cttracks[200], i = 0;
+	do {
+		cin >> ctcars[i] >> cttracks[i];
+		for (int j = 0; j < ctcars[i]; j++) cin >> cars[i][j];
+	} while (ctcars[i++] != 0);
+	i = 0;
+	do {
+		solve(ctcars[i], cttracks[i], cars[i]);
+	} while (ctcars[++i] != 0);
+	delete[] cars;*/
+	int ctcars, cttracks, i = 0;
+	do {
+		cin >> ctcars >> cttracks;
+		for (int j = 0; j < ctcars; j++) cin >> cars[j];
+		solve(ctcars, cttracks);
+	} while (ctcars != 0);
 	return 0;
 }
